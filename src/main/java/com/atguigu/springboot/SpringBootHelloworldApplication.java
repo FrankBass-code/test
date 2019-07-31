4089b19a46a1e81e281ad5c9b0cd3d455081a42e
frank <1575810271@qq.com>
2019-07-31 09:36:24 +0800

    test1

--- src/main/java/com/atguigu/springboot/SpringBootHelloworldApplication.java
+++ src/main/java/com/atguigu/springboot/SpringBootHelloworldApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloworldApplication {
 
 	public static void main(String[] args) {
+		//更新
+		//更新1
 		SpringApplication.run(SpringBootHelloworldApplication.class, args);
 	}
 
