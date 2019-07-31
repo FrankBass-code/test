87cf927b92bd4986e018ae7d7f394dafb480bf65
frank <1575810271@qq.com>
2019-07-31 16:55:36 +0800

    36.登录//有拦截器//拦截器正常//静态资源可访问

--- src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,29 +1,43 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.component.LoginHandlerInterceptor;
 import com.atguigu.springboot.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class MyMvcConfig extends WebMvcConfigurerAdapter {
+public class MyMvcConfig implements WebMvcConfigurer {
 
+    @Override
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/static/");
+    registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+    registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+}
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/atguigu").setViewName("success");
     }
     @Bean
-    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
-        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+    public WebMvcConfigurer webMvcConfigurerAdapter(){
+        WebMvcConfigurer adapter = new WebMvcConfigurer() {
             @Override
             public void addViewControllers(ViewControllerRegistry registry) {
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
                 registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html","/","/user/login","/static/**","/resoureces/**","/public/**","/asserts/**");
             }
         };
         return adapter;
--- src/main/java/com/atguigu/springboot/controller/LoginController.java
+++ src/main/java/com/atguigu/springboot/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Controller
@@ -14,9 +15,11 @@ public class LoginController {
     @RequestMapping(value= "/user/login" , method = RequestMethod.POST)
     public String login(@RequestParam("username") String username,
                         @RequestParam("password") String password,
-                        Map<String, Object> map){
+                        Map<String, Object> map,
+                        HttpSession session){
 
         if(!StringUtils.isEmpty(username)&&"123456".equals(password)){
+            session.setAttribute("loginUser",username);
             return "redirect:/main.html";
 
 
