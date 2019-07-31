30f1b8bc6019e74f2c121cab5079432ff5f0ee83
frank <1575810271@qq.com>
2019-07-31 12:45:27 +0800

    themleaf

--- src/main/java/com/atguigu/springboot/controller/HelloController.java
+++ src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -4,13 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-@ResponseBody
-public class HelloController {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Map;
 
+@Controller
+
+public class HelloController {
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
+
+    }
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map){
+        map.put("hello","<hl>你好</hl>");
+        map.put("users", Arrays.asList("zhangsan","lisi"));
+        return "success";
 
     }
 }
